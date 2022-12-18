bb14bdf1490a5b25b375e3ebf5b7bc52b84a4613
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-18 14:32:05 +0100

    .flatpickr_date_time

---
+++ assets/flatpckr_init.js
@@ -0,0 +1,9 @@
+flatpickr(".flatpickr_date_time",  
+{
+    enableTime: true,
+    altInput: true,    
+    altFormat: "j. F, Y H:i",   
+    time_24hr: true,
+    "locale": "de" 
+    }                 
+);
