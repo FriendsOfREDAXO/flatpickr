a1a5c7f9f715b8b38489f12d959be96f7cc40a6b
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-18 19:17:56 +0100

    Update flatpickr_init.js

--- assets/flatpickr_init.js
+++ assets/flatpickr_init.js
@@ -5,39 +5,57 @@ $(document).on('rex:ready', function () {
         var clocale = element.getAttribute('data-locale') || 'de';
         var cenableTime = element.getAttribute('data-enableTime') || false;
         var caltFormat = element.getAttribute('data-altFormat') || "j. F, Y H:i";
+        var disabled = element.getAttribute('data-disabled') || "";
 
+        if (disabled && disabled != "") {
+            var disabled_list = disabled.split(',');
+        }
+        else { disabled_list = []; }
         flatpickr(element,
             {
                 enableTime: cenableTime,
                 altInput: true,
                 altFormat: caltFormat,
                 time_24hr: true,
+                disable: disabled_list,
                 locale: clocale
             }
         );
     });
 
 
-var pickr_elements2 = document.querySelectorAll('.flatpickr_range');
 
-pickr_elements2.forEach(function (element) {
-    var clocale = element.getAttribute('data-locale') || 'de';
-    var cenableTime = element.getAttribute('data-enableTime') || false;
-    var caltFormat = element.getAttribute('data-altFormat') || "j. F, Y H:i";
-    var rangeField = element.getAttribute('data-rangefield') || "";
-    alert(rangeField);
-    if (rangeField!="") {
-        flatpickr(element,
-           {
-            enableTime: cenableTime,
-            "plugins": [new rangePlugin({ input: rangeField})],
-            altInput: true,
-            altFormat: caltFormat,
-            time_24hr: true,
-            locale: clocale
-        });
-    }
 
-});
+    var pickr_elements2 = document.querySelectorAll('.flatpickr_range');
+
+    pickr_elements2.forEach(function (element) {
+        var clocale = element.getAttribute('data-locale') || 'de';
+        var cenableTime = element.getAttribute('data-enableTime') || false;
+        var caltFormat = element.getAttribute('data-altFormat') || "j. F, Y H:i";
+        var rangeField = element.getAttribute('data-rangefield') || "";
+        var disabled = element.getAttribute('data-disabled') || "";
+
+        if (disabled && disabled != "") {
+            var disabled_list = disabled.split(',');
+        }
+        else { disabled_list = []; }
+        alert(rangeField);
+        if (rangeField != "") {
+            flatpickr(element,
+                {
+                    enableTime: cenableTime,
+                    "plugins": [new rangePlugin({ input: rangeField })],
+                    altInput: true,
+                    altFormat: caltFormat,
+                    time_24hr: true,
+                    disable: disabled_list,
+                    locale: clocale
+                });
+        }
+
+
+
+
+    });
 
 });
