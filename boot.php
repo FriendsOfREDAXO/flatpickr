3cd1cbe2d678a466091791cb36016d2703eae453
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-18 14:47:42 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -3,7 +3,7 @@ $addon = rex_addon::get('flatpickr');
 if (rex::isBackend() && is_object(rex::getUser())) {
     rex_view::addCssFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr.min.css'));
     rex_view::addCssFile($addon->getAssetsUrl('vendor/flatpickr/dist/themes/dark.css'));
-    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/flatpickr.min.js'));
-    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/l10n/de.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr.min.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/l10n/de.js'));
     rex_view::addJsFile($addon->getAssetsUrl('flatpickr_init.js'));
 }
