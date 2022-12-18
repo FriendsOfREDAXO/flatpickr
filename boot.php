521181ee8c02e65ef795ba43666bd0c570dc0923
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-18 14:28:32 +0100

    load assets

--- boot.php
+++ boot.php
@@ -1,1 +1,8 @@
-<?php // Boot code
+<?php
+if (rex::isBackend() && is_object(rex::getUser())) {
+    rex_view::addCssFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr.min.css'));
+    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/themes/dark.css'));
+    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/flatpickr.min.js'));
+    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/l10n/de.js'));
+    rex_view::addJsFile($plyr->getAssetsUrl('flatpckr_init.js'));
+}
