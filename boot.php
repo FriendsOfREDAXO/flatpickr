d0ee979db163cccecf4a9be542eec0dac73e2329
Thomas Skerbis <thomas.skerbis@mac.com>
2022-12-18 14:37:01 +0100

    Set $addon

--- boot.php
+++ boot.php
@@ -1,8 +1,9 @@
 <?php
+$addon = rex_addon::get('flatpckr');
 if (rex::isBackend() && is_object(rex::getUser())) {
-    rex_view::addCssFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr.min.css'));
-    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/themes/dark.css'));
-    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/flatpickr.min.js'));
-    rex_view::addJsFile($plyr->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/l10n/de.js'));
-    rex_view::addJsFile($plyr->getAssetsUrl('flatpckr_init.js'));
+    rex_view::addCssFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr.min.css'));
+    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/themes/dark.css'));
+    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/flatpickr.min.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('vendor/flatpickr/dist/flatpickr/dist/l10n/de.js'));
+    rex_view::addJsFile($addon->getAssetsUrl('flatpckr_init.js'));
 }
