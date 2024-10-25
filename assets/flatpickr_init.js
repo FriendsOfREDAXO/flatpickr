59c8ae135112f85d625d4f4662dec2f32f844eee
Thomas Skerbis <thomas.skerbis@mac.com>
2024-10-26 00:51:44 +0200

    flatpickr neo

--- assets/flatpickr_init.js
+++ assets/flatpickr_init.js
@@ -1,56 +1,104 @@
 $(document).on('rex:ready', function () {
-    var pickr_elements = document.querySelectorAll('.flatpickr');
-
-    pickr_elements.forEach(function (element) {
-        var clocale = element.getAttribute('data-locale') || 'de';
-        var cenableTime = element.getAttribute('data-enableTime') || false;
-        var caltFormat = element.getAttribute('data-altFormat') || "j. F, Y H:i";
-        var disabled = element.getAttribute('data-disabled') || "";
-
-        if (disabled && disabled != "") {
-            var disabled_list = disabled.split(',');
-        }
-        else { disabled_list = []; }
-        flatpickr(element,
-            {
-                enableTime: cenableTime,
-                altInput: true,
-                altFormat: caltFormat,
-                time_24hr: true,
-                disable: disabled_list,
-                locale: clocale
+    // Common callback definitions
+    const commonCallbacks = {
+        weekSelect: function(selectedDates, dateStr, instance) {
+            const weekNumber = selectedDates[0] 
+                ? instance.config.getWeek(selectedDates[0]) 
+                : null;
+            
+            if (instance.element.dataset.weekTarget) {
+                document.querySelector(instance.element.dataset.weekTarget).value = weekNumber;
             }
-        );
-    });
-
-
-
-
-    var pickr_elements2 = document.querySelectorAll('.flatpickr_range');
-
-    pickr_elements2.forEach(function (element) {
-        var clocale = element.getAttribute('data-locale') || 'de';
-        var cenableTime = element.getAttribute('data-enableTime') || false;
-        var caltFormat = element.getAttribute('data-altFormat') || "j. F, Y H:i";
-        var rangeField = element.getAttribute('data-rangefield') || "";
-        var disabled = element.getAttribute('data-disabled') || "";
-
-        if (disabled && disabled != "") {
-            var disabled_list = disabled.split(',');
+            
+            instance.element.dispatchEvent(new CustomEvent('weekSelect', { 
+                detail: { week: weekNumber, date: selectedDates[0] }
+            }));
+            
+            return weekNumber;
         }
-        else { disabled_list = []; }
-        if (rangeField != "") {
-            flatpickr(element,
-                {
-                    enableTime: cenableTime,
-                    "plugins": [new rangePlugin({ input: rangeField })],
-                    altInput: true,
-                    altFormat: caltFormat,
-                    time_24hr: true,
-                    disable: disabled_list,
-                    locale: clocale
+    };
+
+    function parseConfig(configString) {
+        return JSON.parse(configString, function(key, value) {
+            if (typeof value === 'string') {
+                if (value.startsWith('@callback.') && commonCallbacks[value.slice(10)]) {
+                    return commonCallbacks[value.slice(10)];
+                }
+                if (value.startsWith('function(') && value.endsWith('}')) {
+                    return new Function('return ' + value)();
+                }
+            }
+            return value;
+        });
+    }
+
+    document.querySelectorAll('.flatpickr').forEach(function (element) {
+        // Initialize plugins array
+        const plugins = [];
+        
+        // Range Plugin
+        if (element.dataset.rangefield) {
+            plugins.push(new rangePlugin({ 
+                input: element.dataset.rangefield 
+            }));
+        }
+        
+        // Confirm Date Plugin
+        if (element.dataset.confirmDate === 'true') {
+            plugins.push(new confirmDatePlugin({
+                confirmIcon: "<i class='fa fa-check'></i>",
+                confirmText: "OK ",
+                showAlways: true,
+                ...JSON.parse(element.dataset.confirmConfig || '{}')
+            }));
+        }
+        
+        // Month Select Plugin
+        if (element.dataset.monthSelect === 'true') {
+            plugins.push(new monthSelectPlugin({
+                shorthand: true,
+                dateFormat: "Y-m",
+                altFormat: "F Y",
+                ...JSON.parse(element.dataset.monthSelectConfig || '{}')
+            }));
+        }
+        
+        // Week Select Plugin
+        if (element.dataset.weekSelect === 'true') {
+            plugins.push(new weekSelect({
+                ...(element.dataset.weekSelectConfig ? parseConfig(element.dataset.weekSelectConfig) : {})
+            }));
+            
+            // Automatically add week selection callback if not otherwise specified
+            if (!element.dataset.config || !element.dataset.config.includes('"onChange"')) {
+                element.dataset.config = JSON.stringify({
+                    ...JSON.parse(element.dataset.config || '{}'),
+                    onChange: ['@callback.weekSelect']
                 });
+            }
         }
-    });
 
+        // MinMaxTime Plugin
+        if (element.dataset.minMaxTime === 'true') {
+            plugins.push(new minMaxTime({
+                ...(element.dataset.minMaxTimeConfig ? parseConfig(element.dataset.minMaxTimeConfig) : {})
+            }));
+        }
+
+        // Create base config
+        const config = {
+            // Default settings
+            altInput: true,
+            time_24hr: true,
+            locale: 'de',
+            ...(element.dataset.config ? parseConfig(element.dataset.config) : {})
+        };
+
+        // Add plugins if any were configured
+        if (plugins.length > 0) {
+            config.plugins = plugins;
+        }
+        
+        flatpickr(element, config);
+    });
 });
