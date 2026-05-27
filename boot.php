0ae2f4f88e1bc847be824bb34effa2d4c872aa61
Thomas Skerbis <thomas.skerbis@mac.com>
2026-05-27 17:50:32 +0200

    Remove dark.css from backend assets
    Remove dark theme CSS file from backend view.

--- boot.php
+++ boot.php
@@ -2,7 +2,6 @@
 $addon = rex_addon::get('flatpickr');
 if (rex::isBackend() && is_object(rex::getUser())) {
     rex_view::addCssFile($addon->getAssetsUrl('vendor/a11y_datetime/dist/a11y_datetime.min.css'));
-    rex_view::addCssFile($addon->getAssetsUrl('vendor/a11y_datetime/dist/themes/dark.css'));
     rex_view::addJsFile($addon->getAssetsUrl('vendor/a11y_datetime/dist/a11y_datetime.min.js'));
     rex_view::addJsFile($addon->getAssetsUrl('vendor/a11y_datetime/dist/l10n/de.js'));
     rex_view::addJsFile($addon->getAssetsUrl('vendor/a11y_datetime/dist/plugins/rangePlugin.js'));
